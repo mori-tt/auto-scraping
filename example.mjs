540c2123e53707a144c511d17fce88e6b22e7737
T_Mori <morit0301@gmail.com>
2024-12-13 15:45:03 +0900

    Getting elements with locator

--- example.mjs
+++ example.mjs
@@ -5,9 +5,21 @@ import { chromium } from "@playwright/test";
 
   const page = await browser.newPage();
   await page.goto("http://localhost:3000");
-  const htmlStr = await page.content();
 
-  console.log(htmlStr);
+  // CSSセレクター
+  const pageTitleLocator = page.locator(".navbar-brand");
+  const pageTitle = await pageTitleLocator.innerText();
+  // console.log(pageTitle);
+
+  // 文字列で要素を取得
+  const textLocator = page.locator("text=名刺管理アプリ");
+  const pageText = await textLocator.innerText();
+  // console.log(pageText);
+
+  // xpathで要素を取得
+  const xpathLocator = page.locator('xpath=//*[@id="__next"]/nav/div/a');
+  const pageXpath = await xpathLocator.innerText();
+  console.log(pageXpath);
 
   await browser.close();
 })();
