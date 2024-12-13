6f422eb8c0c869a7606254eb93d8e2e7b57d00f4
T_Mori <morit0301@gmail.com>
2024-12-13 16:49:50 +0900

    Advanced use of locator to get elements (using selector chains, etc.)

--- example.mjs
+++ example.mjs
@@ -6,7 +6,7 @@ import { chromium } from "@playwright/test";
   const page = await browser.newPage();
   await page.goto("http://localhost:3000");
 
-  // CSSセレクター
+  // CSSセレクターでの要素の取得
   const pageTitleLocator = page.locator(".navbar-brand");
   const pageTitle = await pageTitleLocator.innerText();
   // console.log(pageTitle);
@@ -19,7 +19,24 @@ import { chromium } from "@playwright/test";
   // xpathで要素を取得
   const xpathLocator = page.locator('xpath=//*[@id="__next"]/nav/div/a');
   const pageXpath = await xpathLocator.innerText();
-  console.log(pageXpath);
+  // console.log(pageXpath);
+
+  //  CSSセレクターでの要素の取得の応用1
+  const pageTitleLocator2 = page.locator(".cards.list-group-item > a >> nth=2");
+  const pageTitle2 = await pageTitleLocator2.innerText();
+  // console.log(pageTitle2);
+
+  //  CSSセレクターでの要素の取得の応用2
+  const pageTitleLocator3 = page.locator(
+    ".cards.list-group-item:nth-child(3)>a"
+  );
+  const pageTitle3 = await pageTitleLocator3.innerHTML();
+  // console.log(pageTitle3);
+
+  //  CSSセレクターでの要素の取得の応用3 親要素を取得
+  const pageTitleLocator4 = page.locator(".cards.list-group-item >> ..");
+  const pageTitle4 = await pageTitleLocator4.innerHTML();
+  console.log(pageTitle4);
 
   await browser.close();
 })();
