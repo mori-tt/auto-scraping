014b98cb0b815910397fb90e3436b8db474db46b
T_Mori <morit0301@gmail.com>
2024-12-15 04:06:32 +0900

    Write process to file (Write down the first 10 names and company names displayed)

---
+++ example5.mjs
@@ -0,0 +1,38 @@
+// ファイルへの書き込み処理 一番最初に表示されている10件を書き込む 会社名を追加
+import { chromium } from "@playwright/test";
+import * as fs from "fs";
+import { Parser } from "json2csv";
+
+(async () => {
+  const browser = await chromium.launch({ headless: false, slowMo: 500 });
+
+  const page = await browser.newPage();
+  await page.goto("http://localhost:3000");
+
+  const cardLocators = page.locator(".cards.list-group-item .name");
+
+  const cardCount = await cardLocators.count();
+
+  const fetchedCards = [];
+  for (let i = 0; i < cardCount; i++) {
+    const cardLocator = cardLocators.locator(`nth=${i}`);
+
+    await cardLocator.click();
+    const companyLocator = await page.locator(".card-title.company");
+    const companyText = await companyLocator.textContent();
+
+    const nameLocator = await page.locator(".card-text.name");
+    const nameText = await nameLocator.textContent();
+    fetchedCards.push({ company: companyText, name: nameText });
+
+    const backLocator = page.locator("text=戻る");
+    await backLocator.click();
+  }
+
+  const parser = new Parser();
+
+  const csv = parser.parse(fetchedCards);
+  fs.writeFileSync("./text-data.csv", csv);
+
+  await browser.close();
+})();
